222dbc2833db21848b698b0e9aa55a5fe2aee0e2
Nicolas <mohamedsabirr2000@gmail.com>
2025-02-04 23:11:52 +0300

    Finel Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AlexaImg from "./images/alexa.png";
 import CortanaImg from "./images/cortana.png";
@@ -6,10 +7,43 @@ import SiriImg from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div>Pesonal Degital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" img={AlexaImg} />
-      <ProfileCard title="Cortana" handle="@cortana32" img={CortanaImg} />
-      <ProfileCard title="Siri" handle="@siri01" img={SiriImg} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Pesonal Degital Assistants</p>
+        </div>
+      </section>
+
+      <div></div>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                img={AlexaImg}
+                description="Alexa is a digital assistant created by Amazon and is part of the Alexa family of products."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                img={CortanaImg}
+                description="Cortana is a digital assistant created by Microsoft and is part of the Cortana family of products."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                img={SiriImg}
+                description="Siri is a digital assistant created by Apple and is part of the Siri family of products."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,18 @@
-function ProfileCard({ title, handle, img }) {
+function ProfileCard({ title, handle, img, description }) {
   return (
-    <div>
-      <img alt="profile" src={img} />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
-      {/* <div>Handle is {img}</div> */}
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img alt="pda logo" src={img} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
